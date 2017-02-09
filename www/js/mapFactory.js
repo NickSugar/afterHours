99151a6930f73a397264b9e1e73c712fb274f8d9
NickSugar <nicksugar1@outlook.com>
2017-02-09 13:46:27 -0700

    got all 20 markers and marker clusters to populate on the map

--- www/js/mapFactory.js
+++ www/js/mapFactory.js
@@ -9,81 +9,55 @@
 
 
   function mapService($http, $cordovaGeolocation) {
+    let vm = this;
     let service = {};
+    
     service.mapInit = function (userLocation, places) {
 
               let map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 16,
+                zoom: 14,
                 center: {lat: userLocation.coords.latitude, lng: userLocation.coords.longitude}
               });
 
-              // Create an array of alphabetical characters used to label the markers.
-              let labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+              var markers = [];
 
-              // Add some markers to the map.
-              // Note: The code uses the JavaScript Array.prototype.map() method to
-              // create an array of markers based on a given "locations" array.
-              // The map() method here has nothing to do with the Google Maps API.
-
-              var locations = [];
-              for (var i = 0; i < places.data.results.length; i++) {
-                let latLngObject = {}
-                latLngObject.lat = places.data.results[i].geometry.location.lat;
-                latLngObject.lng = places.data.results[i].geometry.location.lng;
-                locations.push(latLngObject)
-              }
-
-              // console.log(places);
-              var markers = locations.map(function(location, i) {
-
-                return new google.maps.Marker({
-                  position: location,
-                  label: labels[i % labels.length]
-                });
-              });
-
-              var infowindow = new google.maps.InfoWindow();
-              var placeService = new google.maps.places.PlacesService(map);
-
-
-
+              var counter = 0;
               function detailsCallback (place, status) {
+
+                let labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+                var infowindow = new google.maps.InfoWindow();
+
                 if (status === google.maps.places.PlacesServiceStatus.OK) {
-                  google.maps.event.addListener(markers[j], 'click', function() {
+                  var marker = new google.maps.Marker({
+                    map: map,
+                    position: place.geometry.location,
+                    label: labels[counter]
+                  });
+                  markers.push(marker);
+
+                  google.maps.event.addListener(marker, 'click', function() {
                     infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
                       'Place ID: ' + place.place_id + '<br>' +
                       place.formatted_address + '</div>');
                     infowindow.open(map, this);
                   });
                 }
-              }
-              for (var j = 0; j < markers.length; j++) {
-                var request = {
-                  placeId: places.data.results[j].id
-                };
-                placeService.getDetails(request, detailsCallback);
+                counter++;
               }
 
-
-              var markerCluster = new MarkerClusterer(map, markers, {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
-
-              // Add a marker clusterer to manage the markers.
-
+              var placeService = new google.maps.places.PlacesService(map);
+              var clear = setInterval(function () {
+                if (counter < 20) {
+                  var request = {
+                    placeId: places.data.results[counter].place_id
+                  };
+                  placeService.getDetails(request, detailsCallback);
+                }else {
+                  var markerCluster = new MarkerClusterer(map, markers, {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+                  clearInterval(clear);
+                }
+              }, 700);
             };
-
     return service;
   }
-
 })();
-
-
-
-
-// var marker = new google.maps.Marker({
-//   map:map,
-//   origin: new google.maps.Point(0, 0),
-//   clickable: true,
-//   anchor: new google.maps.Point(-17, 0),
-//   position: new google.maps.LatLng(data.coord.lat, data.coord.lon),
-//   icon: iconImgUrl, // null = default icon
-//   zIndex:99999999 //to get the icon on the forefront
